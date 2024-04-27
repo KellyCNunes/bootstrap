adc731958895c361773ac1ec36dff5c1378cb571
KellyCNunes <kelly.crisnunes@hotmail.com>
2024-04-27 11:10:31 -0300

    27/04 atua

---
+++ modularizar/funcoes.php
@@ -0,0 +1,20 @@
+<?php
+     
+    function exibirData(){
+        echo "Hoje é dia ".date("d");
+    }
+
+    function somar($n1, $n2){
+        return $n1 + $n2;
+    }
+
+
+    function positivoNegativo($v){
+        if ($v > 0){
+            return "Valor positivo";
+        } elseif($v<0){
+            return "Valor Negativo";
+        } else{
+            return "Ihual a zero";
+        }
+    }
