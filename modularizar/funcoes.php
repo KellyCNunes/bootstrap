44059051f364146aff93ec6c8b0fe03d47dc61f2
KellyCNunes <kelly.crisnunes@hotmail.com>
2024-05-04 10:22:56 -0300

    04/05

--- modularizar/funcoes.php
+++ modularizar/funcoes.php
@@ -18,3 +18,39 @@
             return "Ihual a zero";
         }
     }
+
+    function menorValor($v, $v1, $v2, $v3, $v4, $v5, $v6, $v7){
+        $menorvalor = 999999999999999999999999999999999;
+        $pos = 0;
+
+        if ($v<$menorvalor){
+            $menorvalor = $v1;
+            $pos = 1;
+        }
+        if ($v2<$menorvalor){
+            $menorvalor = $v2;
+            $pos = 2;
+        }
+        if ($v3<$menorvalor){
+            $menorvalor = $v3;
+            $pos = 3;
+        }
+        if ($v4<$menorvalor){
+            $menorvalor = $v4;
+            $pos = 4;
+        }
+        if ($v5<$menorvalor){
+            $menorvalor = $v5;
+            $pos = 5;
+        }
+        if ($v6<$menorvalor){
+            $menorvalor = $v6;
+            $pos = 6;
+        }
+        if ($v7<$menorvalor){
+            $menorvalor = $v7;
+            $pos = 7;
+        }
+
+        return array ($menorvalor, $pos);
+    }
